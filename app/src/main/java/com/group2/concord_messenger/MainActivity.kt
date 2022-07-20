f3bfd6ea73b7111a41e1ed839605ebce7a838780
Dylan <dylanbingmanners@gmail.com>
2022-07-19 21:06:33 -0700

    Added layout for login

--- app/src/main/java/com/group2/concord_messenger/MainActivity.kt
+++ app/src/main/java/com/group2/concord_messenger/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.group2.concord_messenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
