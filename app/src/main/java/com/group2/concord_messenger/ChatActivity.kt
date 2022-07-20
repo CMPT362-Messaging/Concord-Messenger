408e1b87f5fcceae57584e35da08cb7b294fd032
trevorbonas <trevorbonas@gmail.com>
2022-07-19 22:00:20 -0700

    Add ChatActivity & chat layouts

---
+++ app/src/main/java/com/group2/concord_messenger/ChatActivity.kt
@@ -0,0 +1,11 @@
+package com.group2.concord_messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
