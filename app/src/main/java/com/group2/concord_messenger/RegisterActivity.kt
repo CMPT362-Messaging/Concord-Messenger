57a1d4f75e61aed11d62fca962640dcfd7c57a6a
Dylan <dylanbingmanners@gmail.com>
2022-07-20 00:17:27 -0700

    Add layout for registration

---
+++ app/src/main/java/com/group2/concord_messenger/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.group2.concord_messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
