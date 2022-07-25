0999066bdcb3b60c9437f26a489409a5859adb42
trevorbonas <trevorbonas@gmail.com>
2022-07-24 19:40:06 -0700

    Add Firebase dependencies

--- app/src/main/java/com/group2/concord_messenger/MainActivity.kt
+++ app/src/main/java/com/group2/concord_messenger/MainActivity.kt
@@ -9,5 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+
     }
 }
