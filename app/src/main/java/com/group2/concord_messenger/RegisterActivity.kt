15c0f4949c51a72837a9164dfa85f1dc67107c1c
Dylan <dylanbingmanners@gmail.com>
2022-07-28 12:25:32 -0700

    Add email password registration

--- app/src/main/java/com/group2/concord_messenger/RegisterActivity.kt
+++ app/src/main/java/com/group2/concord_messenger/RegisterActivity.kt
@@ -2,12 +2,142 @@ package com.group2.concord_messenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.*
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class RegisterActivity : AppCompatActivity()
+class RegisterActivity : AppCompatActivity(), View.OnClickListener
 {
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var usernameInputView: EditText
+    private lateinit var emailInputView: EditText
+    private lateinit var passInputView: EditText
+    private lateinit var passConfirmationInputView: EditText
+    private lateinit var createAccountButtonView: Button
+    private lateinit var backButtonView: Button
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        // Get views
+        usernameInputView = findViewById(R.id.register_edittext_username)
+        emailInputView = findViewById(R.id.register_edittext_email)
+        passInputView = findViewById(R.id.register_edittext_password)
+        passConfirmationInputView = findViewById(R.id.register_edittext_confirm_password)
+        createAccountButtonView = findViewById(R.id.register_button_create_account)
+        backButtonView = findViewById(R.id.register_button_back)
+
+        createAccountButtonView.setOnClickListener(this)
+        backButtonView.setOnClickListener(this)
+
+        firebaseAuth = Firebase.auth
+    }
+
+    override fun onClick(view: View?)
+    {
+        when(view?.id)
+        {
+            // CREATE ACCOUNT BUTTON
+            R.id.register_button_create_account ->
+            {
+                val username = usernameInputView.text.toString()
+                val email = emailInputView.text.toString()
+                val password = passInputView.text.toString()
+                val passwordConfirm = passConfirmationInputView.text.toString()
+
+                createAccount(username, email, password, passwordConfirm)
+            }
+
+            // BACK BUTTON
+            R.id.register_button_back ->
+            {
+                println("DEBUG ${Firebase.auth.currentUser?.email} ${Firebase.auth.currentUser?.displayName}")
+            }
+        }
+    }
+
+    private fun createAccount(username: String, email: String, password: String, password_confirm: String)
+    {
+        // Clear old errors from input fields
+        clearFieldErrors(listOf<EditText>(
+            usernameInputView,
+            emailInputView,
+            passInputView,
+            passConfirmationInputView,
+        ))
+
+        //
+        if(username.isBlank() || email.isBlank() || password.isBlank() ||
+            password_confirm.isBlank() || password != password_confirm)
+        {
+            if(username.isBlank())
+            {
+                showFieldError(usernameInputView, resources.getString(R.string.login_error_empty_field))
+            }
+            else if(email.isBlank())
+            {
+                showFieldError(emailInputView, resources.getString(R.string.login_error_empty_field))
+            }
+            else if(password.isBlank())
+            {
+                showFieldError(passInputView, resources.getString(R.string.login_error_empty_field))
+            }
+            else if(password_confirm.isBlank())
+            {
+                showFieldError(passConfirmationInputView, resources.getString(R.string.login_error_empty_field))
+            }
+            else if(password != password_confirm)
+            {
+                showFieldError(passConfirmationInputView, resources.getString(R.string.login_error_password_not_match))
+            }
+        }
+        else
+        {
+            firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener()
+            {
+                authResult ->
+                if(authResult.isSuccessful)
+                {
+                    // Registration successful
+                }
+                else
+                {
+                    when(authResult.exception!!::class)
+                    {
+                        FirebaseAuthInvalidCredentialsException::class ->
+                            showFieldError(emailInputView, resources.getString(R.string.login_error_invalid_email))
+
+                        FirebaseAuthUserCollisionException::class ->
+                            showFieldError(emailInputView, resources.getString(R.string.login_error_account_exists))
+
+                        FirebaseAuthWeakPasswordException::class ->
+                            showFieldError(passInputView, resources.getString(R.string.login_error_invalid_password))
+
+                        else -> {authResult.exception!!.printStackTrace()}
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showFieldError(view: EditText, message: String)
+    {
+        view.error = message
+    }
+
+    private fun clearFieldErrors(views: List<EditText>)
+    {
+        for(view in views)
+        {
+            view.error = null
+        }
     }
 }
